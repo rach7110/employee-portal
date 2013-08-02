b4c25112363c19fe20099211a303cc1de0f2f64a
Sonny Gonzalez <sonnyg.email@gmail.com>
2013-08-02 14:54:55 -0500

    initial commit

--- app/models/message.rb
+++ app/models/message.rb
@@ -5,4 +5,14 @@ class Message < ActiveRecord::Base
   belongs_to :sender, class_name: 'User', foreign_key: 'sender_id'
 
   scope :unread, -> { where(read: false) }
+
+
+def deliver!(args)
+  sending  = args[:sending]
+  receving = args[:receiving]
+
+  self.sender = sending
+  # raise ArgumentError, "both sender and receiver must be specified" unless sending && receiving
+end
+
 end
--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,5 +1,38 @@
 require 'spec_helper'
 
 describe Message do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '.deliver!' do
+    let(:message)   { Message.new(body: 'Hello, there!') }
+    let(:s) { User.new }
+    let(:r) { User.new }
+
+    context 'when both the sender and recipient are specified' do
+      it 'should set the sender of the message' do
+        message.deliver! sending: s, receiving: r
+        expect { message.sender }.to eq(s) 
+      end
+
+      it 'should set the recipient of the message' do
+      end
+
+      it 'should persist the message to the database' do
+      end
+    end
+
+    context 'when neither the sender or recipient is specified' do
+      it 'should raise an error' do
+      end
+    end
+
+    context 'when only the sender is specified' do
+      it 'should raise an error' do
+      end
+    end
+
+    context 'when only the recipient is specified' do
+      it 'should raise an error' do
+      end
+    end
+
+  end
 end
